dd0b879e33642bc38fea8fcca6769e5d0784efa2
ankittkinha <ankittkinha@gmail.com>
2023-07-06 21:57:13 +0530

    used contenteditable

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-
 const speechText = document.querySelector(".speechText");
-const btn = document.querySelector(".btn")
-
+const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", function () {
-
-    const output = document.querySelector(".words")
-    const btn = document.querySelector(".btn")
+    const output = document.querySelector(".words");
+    const btn = document.querySelector(".btn");
 
     let recognization = new webkitSpeechRecognition();
     recognization.onstart = () => {
-        btn.innerHTML = "Listening..."
+        btn.innerHTML = "Listening...";
     }
     recognization.onresult = (e) => {
         let transcript = e.results[0][0].transcript;
         output.innerHTML +=  `<p>${transcript}</p>`;
-        // output.classList.remove("hide")
     }
     recognization.onaudioend = (e) => {
         btn.innerHTML = "Speech To Text";
+        output.classList.add("showDisplay");
     }
     
-    recognization.start();
-})
+    recognization.start();   
+});
